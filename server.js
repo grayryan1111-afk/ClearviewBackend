135e3530619420c12c0a13f43e8c0d961c9a1004
grayryan1111-afk <grayryan1111@gmail.com>
2025-11-29 04:01:19 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import openaiRoutes from "./routes/openai.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
+
+app.get("/", (req, res) => {
+  res.send("Backend running!");
+});
+
+// OpenAI Routes
+app.use("/api/openai", openaiRoutes);
+
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
